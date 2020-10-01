7cf15c2d289c1845a04bbbcdb943854c92b0a2b4
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-10-01 11:13:32 +0530

    taking user input

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class TicTacToeGame
 {
+	//uc1
 	public char[] createBoard()
 	{
 		char[] board = new char[10];
@@ -9,9 +11,28 @@ public class TicTacToeGame
 		}
 		return board;
 	}
+
+	//Uc2
+	public char chooseInput(Scanner userInput)
+	{
+		System.out.println("Enter Your choice:(X or o) ");
+		return userInput.next().toUpperCase().charAt(0);
+	}
+
 	public static void main(String[] args)
 	{
+		char computerletter;
 		TicTacToeGame tictactoe = new TicTacToeGame();
-		tictactoe.createBoard();
+		char [] board = tictactoe.createBoard();
+		Scanner userinput = new Scanner(System.in);
+		char userletter = tictactoe.chooseInput(userinput);
+		if ( userletter == 'X' )
+		{
+			computerletter = 'O';
+		}
+		else
+		{
+			computerletter = 'X' ;
+		}
 	}
 }
